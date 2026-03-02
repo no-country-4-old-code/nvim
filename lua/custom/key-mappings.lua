ca586d6f1a596f11e51accbe8dffa5307ba7ffde
no-country-4-old-code <koalmori@LDED-GTXQ954.corp.draeger.global>
2026-03-02 15:39:36 +0100

    add go to definition / reference / usages / call-stack stuff

--- lua/custom/key-mappings.lua
+++ lua/custom/key-mappings.lua
@@ -14,6 +14,12 @@ function M.setup()
 
 	-- telescope keymaps
 	local telescope = require("telescope.builtin")
+
+	-- code navigation (lsp)
+	vim.keymap.set("n", "<leader>cd", telescope.lsp_definitions, { desc = "lsp go to definition" })
+	vim.keymap.set("n", "<leader>cu", telescope.lsp_references, { desc = "lsp usages / references" })
+	vim.keymap.set("n", "<leader>ci", telescope.lsp_incoming_calls, { desc = "lsp incoming calls (callers)" })
+	vim.keymap.set("n", "<leader>co", telescope.lsp_outgoing_calls, { desc = "lsp outgoing calls (callees)" })
 	vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "telescope find files" })
 	vim.keymap.set(
 		"n",
