3813843f5c565760d75318aee7b7084516bc4b81
no-country-4-old-code <koalmori@LDED-GTXQ954.corp.draeger.global>
2026-03-02 16:27:05 +0100

    add file live grep which support extensions

--- lua/custom/key-mappings.lua
+++ lua/custom/key-mappings.lua
@@ -15,18 +15,27 @@ function M.setup()
 	-- telescope keymaps
 	local telescope = require("telescope.builtin")
 
+	-- helper
+	local function grep_by_extensions()
+		local input = vim.fn.input("Extensions (e.g. c cs json): ")
+		if input == "" then
+			return
+		end
+		local globs = {}
+		for ext in input:gmatch("%S+") do
+			table.insert(globs, "*." .. ext)
+		end
+		telescope.live_grep({ glob_pattern = globs })
+	end
+
 	-- code navigation (lsp)
 	vim.keymap.set("n", "<leader>cd", telescope.lsp_definitions, { desc = "lsp go to definition" })
 	vim.keymap.set("n", "<leader>cu", telescope.lsp_references, { desc = "lsp usages / references" })
 	vim.keymap.set("n", "<leader>ci", telescope.lsp_incoming_calls, { desc = "lsp incoming calls (callers)" })
 	vim.keymap.set("n", "<leader>co", telescope.lsp_outgoing_calls, { desc = "lsp outgoing calls (callees)" })
 	vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "telescope find files" })
-	vim.keymap.set(
-		"n",
-		"<leader>fg",
-		":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-		{ desc = "telescope live grep" }
-	)
+	vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "telescope live grep" })
+	vim.keymap.set("n", "<leader>fe", grep_by_extensions, { desc = "telescope grep filtered by extension" })
 	vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "telescope buffers" })
 	vim.keymap.set("n", "<leader>fj", telescope.jumplist, { desc = "telescope list jumps" })
 	vim.keymap.set("n", "<leader>gc", telescope.git_commits, { desc = "telescope shows git commits" })
