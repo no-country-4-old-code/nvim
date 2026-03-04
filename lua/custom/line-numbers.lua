56bccd8c5540af281107960dcce062c1111cfb76
no-country-4-old-code <koalmori@LDED-GTXQ954.corp.draeger.global>
2026-03-04 23:09:32 +0100

    make sure line numbers does not change when entering insert mode

--- lua/custom/line-numbers.lua
+++ lua/custom/line-numbers.lua
@@ -21,11 +21,13 @@ function M.setup()
 	vim.api.nvim_create_autocmd("InsertEnter", {
 		callback = function()
 			vim.opt.relativenumber = false
+			vim.opt.number = true
 		end,
 	})
 	vim.api.nvim_create_autocmd("InsertLeave", {
 		callback = function()
 			vim.opt.relativenumber = true
+			vim.opt.number = true
 		end,
 	})
 end
