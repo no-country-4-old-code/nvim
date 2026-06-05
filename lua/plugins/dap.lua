9addcabf3927ebb60a49a62dd26f760c9e58ae92
no-country-4-old-code <koalmori@LDED-GTXQ954.corp.draeger.global>
2026-06-05 10:41:16 +0200

    add debug adapter plugin

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,82 @@
+-- Debug Adapter Protocol (DAP) stack for C/C++ using GDB (cpptools via Mason)
+-- Keymaps: <leader>d* (see below) - all shown in <leader>h keymap browser
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",          -- required by nvim-dap-ui
+			"theHamsta/nvim-dap-virtual-text", -- inline variable values
+			"jay-babu/mason-nvim-dap.nvim",   -- bridges Mason <-> nvim-dap
+			"mason-org/mason.nvim",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "cpptools" },
+				automatic_installation = true,
+				handlers = {
+					function(config)
+						require("mason-nvim-dap").default_setup(config)
+					end,
+				},
+			})
+
+			-- mason-nvim-dap's default handler points cppdbg at exepath('OpenDebugAD7'),
+			-- which is empty since Mason packages aren't on $PATH; register explicitly.
+			-- get_install_path() was removed in Mason 2.0, so use stdpath("data")/mason directly.
+			local install_path = vim.fn.stdpath("data") .. "/mason/packages/cpptools"
+			local binary = vim.fn.glob(install_path .. "/**/OpenDebugAD7", false, true)[1]
+			if binary then
+				dap.adapters.cppdbg = { id = "cppdbg", type = "executable", command = binary }
+			end
+
+			-- inline variable values next to the code
+			require("nvim-dap-virtual-text").setup({})
+
+			-- panels: scopes, breakpoints, call stack, watches, ...
+			dapui.setup()
+
+			-- auto-open / auto-close the UI panels when a session starts/ends
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			-- C launch config (prompts for executable path, stops at main)
+			dap.configurations.c = {
+				{
+					name = "Launch (gdb)",
+					type = "cppdbg",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+					stopAtEntry = true,
+					MIMode = "gdb",
+				},
+			}
+			-- reuse the same config for C++
+			dap.configurations.cpp = dap.configurations.c
+
+			-- keymaps -------------------------------------------------------
+			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint (debug)" })
+			vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue / start (debug)" })
+			vim.keymap.set("n", "<leader>dn", dap.step_over, { desc = "Step over (debug)" })
+			vim.keymap.set("n", "<leader>ds", dap.step_into, { desc = "Step into (debug)" })
+			vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "Step out (debug)" })
+			vim.keymap.set("n", "<leader>dx", dap.terminate, { desc = "Terminate (debug)" })
+			vim.keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "Toggle REPL (debug)" })
+			vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Toggle debug UI (debug)" })
+			vim.keymap.set("n", "<leader>de", dapui.eval, { desc = "Evaluate expression (debug)" })
+		end,
+	},
+}
