1dc52bf82805ae0a140d84ebd9fe7dfb2b9b9ed0
no-country-4-old-code <koalmori@LDED-GTXQ954.corp.draeger.global>
2026-06-08 09:34:24 +0200

    add shortcut for faster debugging

--- example/.nvim.lua
+++ example/.nvim.lua
@@ -23,4 +23,28 @@ vim.keymap.set("n", "<leader>4", function()
 	print("Moin Moin")
 end, { desc = "Moin Moin" })
 
--- Add more shortcuts here (<leader>5 .. <leader>9)
+-- Example: C project
+vim.keymap.set("n", "<leader>5", function()
+	vim.cmd("w")
+	vim.cmd("!gcc main.c -o main -g")
+end, { desc = "compile main.c" })
+
+vim.keymap.set("n", "<leader>6", function()
+	vim.cmd("w")
+	local result = vim.fn.system("gcc main.c -o main -g")
+	if vim.v.shell_error ~= 0 then
+		vim.notify("Compile failed:\n" .. result, vim.log.levels.ERROR)
+		return
+	end
+	require("dap").run({
+		name = "Launch (gdb)",
+		type = "cppdbg",
+		request = "launch",
+		program = vim.fn.getcwd() .. "/main",
+		cwd = "${workspaceFolder}",
+		stopAtEntry = true,
+		MIMode = "gdb",
+	})
+end, { desc = "compile & debug main.c" })
+
+-- Add more shortcuts here (<leader>7 .. <leader>9)
