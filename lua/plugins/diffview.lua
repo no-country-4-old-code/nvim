9955941ce202d42a65fa99fbc47b8c789f449df6
no-country-4-old-code <koalmori@LDED-GTXQ954.corp.draeger.global>
2026-06-08 16:25:52 +0200

    add keymaps to diff view

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,5 +3,18 @@ return {
 	cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
 	opts = {
 		use_icons = false,
+		keymaps = {
+			view = {
+				{ "n", "]h", function() vim.cmd("normal! ]c") end, { desc = "Next hunk" } },
+				{ "n", "[h", function() vim.cmd("normal! [c") end, { desc = "Prev hunk" } },
+			},
+		},
+		hooks = {
+			diff_buf_win_enter = function(bufnr, winid, ctx)
+				vim.opt_local.foldmethod = "diff"
+				vim.opt_local.foldlevel = 0
+				vim.cmd("normal! zM")
+			end,
+		},
 	},
 }
