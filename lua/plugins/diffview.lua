aaea2b51a32a40b2de5a6a5aa233c0490a98c5cc
no-country-4-old-code <koalmori@LDED-GTXQ954.corp.draeger.global>
2026-06-09 18:42:47 +0200

    refactor diffview and git signs

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -5,41 +5,57 @@ return {
 		use_icons = false,
 		keymaps = {
 			view = {
-				{ "n", "]h", function() vim.cmd("normal! ]c") end, { desc = "Next hunk" } },
-				{ "n", "[h", function() vim.cmd("normal! [c") end, { desc = "Prev hunk" } },
-				{ "n", "<leader>gp", function() vim.cmd("diffput") end, { desc = "Push hunk to other panel" } },
-				{ "n", "<leader>gl", function() vim.cmd("diffget") end, { desc = "Get hunk from other panel" } },
+				-- merging changes
 				{
-					"n", "<leader>ga",
-					function() require("gitsigns").stage_hunk() end,
-					{ desc = "Stage hunk (git)" },
+					"n",
+					"<leader>gp",
+					function()
+						vim.cmd("diffput")
+					end,
+					{ desc = "Git-Diff : Push hunk to other panel" },
+				},
+				{ "v", "<leader>gp", ":diffput<CR>", { desc = "Git-Diff : Push selected lines to other panel" } },
+				{
+					"n",
+					"<leader>gl",
+					function()
+						vim.cmd("diffget")
+					end,
+					{ desc = "Git-Diff : Get hunk from other panel" },
+				},
+
+				-- git staging
+				{ "v", "<leader>gl", ":diffget<CR>", { desc = "Git-Diff : Get selected lines from other panel" } },
+				{
+					"n",
+					"<leader>ga",
+					function()
+						require("gitsigns").stage_hunk()
+					end,
+					{ desc = "Git-Changes : Stage hunk (git)" },
 				},
 				{
-					"v", "<leader>ga",
+					"v",
+					"<leader>ga",
 					function()
 						local l1 = vim.fn.line("'<")
 						local l2 = vim.fn.line("'>")
 						require("gitsigns").stage_hunk({ l1, l2 })
 					end,
-					{ desc = "Stage selected lines (git)" },
+					{ desc = "Git-Changes : Stage selected lines (git)" },
 				},
 				{
-					"n", "<leader>gu",
-					function() require("gitsigns").undo_stage_hunk() end,
-					{ desc = "Unstage hunk (git)" },
+					"n",
+					"<leader>gu",
+					function()
+						require("gitsigns").undo_stage_hunk()
+					end,
+					{ desc = "Git-Changes : Unstage hunk (git)" },
 				},
-				{ "n", "<leader>ginfo", function() end, { desc = "Diffview status: ga=stage hunk | gu=unstage hunk | gA=stage file | gU=unstage file | S=stage all | U=unstage all | - toggle stage | X=discard" } },
 				{ "n", "zo", "zo", { desc = "Open fold" } },
-				{ "n", "zr", "zr", { desc = "Open all folds one level" } },
-				{ "v", "<leader>gp", ":diffput<CR>", { desc = "Push selected lines to other panel" } },
-				{ "v", "<leader>gl", ":diffget<CR>", { desc = "Get selected lines from other panel" } },
+				{ "n", "zc", "zc", { desc = "Close fold" } },
 			},
-			file_panel = {
-				{ "n", "<leader>ga", "<Cmd>Gitsigns stage_hunk<CR>", { desc = "Stage hunk under cursor (git)" } },
-				{ "n", "<leader>gA", function() require("gitsigns").stage_buffer() end, { desc = "Stage entire file (git)" } },
-				{ "n", "<leader>gU", function() require("gitsigns").reset_buffer_index() end, { desc = "Unstage entire file (git)" } },
-				{ "n", "<leader>ginfo", function() end, { desc = "File panel: - toggle stage | S stage all | U unstage all | X discard | gA stage file | gU unstage file" } },
-			},
+			file_panel = {},
 		},
 		hooks = {
 			diff_buf_win_enter = function(bufnr, winid, ctx)
