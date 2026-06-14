579d40f3f3d9507d1ef60aa782804d5e61104f55
no-country-4-old-code <koalmori@LDED-GTXQ954.corp.draeger.global>
2026-06-14 15:34:52 +0200

    cppcheck not on write

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -3,30 +3,9 @@ return {
 	event = { "BufReadPost", "BufWritePost" },
 	config = function()
 		local lint = require("lint")
-		lint.linters_by_ft = {
-			c   = { "cppcheck" },
-			cpp = { "cppcheck" },
-		}
+		lint.linters_by_ft = {}
 
-		-- Per-file cppcheck on save, using compile_commands.json when available.
-		-- For a project-wide check bound to a key, see lua/custom/cppcheck.lua.
-		lint.linters.cppcheck.append_fname = false
-		lint.linters.cppcheck.args = {
-			"--enable=warning,style,performance,information",
-			"--language=c++",
-			"--inline-suppr",
-			"--quiet",
-			"--suppress=missingIncludeSystem",
-			"--template={file}:{line}:{column}: [{id}] {severity}: {message}",
-			function()
-				-- auto-discover compile_commands.json one directory below cwd
-				local db = vim.fn.globpath(vim.fn.getcwd(), "*/compile_commands.json", 0, 1)
-				if db[1] then return "--project=" .. db[1] end
-			end,
-			function()
-				return "--file-filter=" .. vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.")
-			end,
-		}
+		-- For a project-wide cppcheck check bound to a key, see lua/custom/cppcheck.lua.
 
 		vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
 			callback = function()
